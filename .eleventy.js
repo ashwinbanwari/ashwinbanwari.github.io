929d53d85d17fe6678699e5ad21110ec3cb4206e
Ashwin Kishin Banwari <ashwinkbanwari@gmail.com>
2024-05-12 02:15:28 -0700

    lair v1

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/somya/Build");
     eleventyConfig.addPassthroughCopy("src/somya/TemplateData");
+    eleventyConfig.addPassthroughCopy("src/index.css")
+    eleventyConfig.addPassthroughCopy("src/ip.js")
     return {
         dir: {
             input: 'src',
